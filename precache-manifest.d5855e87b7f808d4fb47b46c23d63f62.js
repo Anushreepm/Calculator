cd3d96941a15d65e403c70b9f6ee2732c817eae5
Anushree <anushreepm13@gmail.com>
2020-07-21 21:47:09 +0530

    Updates

---
+++ precache-manifest.d5855e87b7f808d4fb47b46c23d63f62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ad1f3dd944b6dea5558c995bdee1a9",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "a4f13930acf3e8170e41",
+    "url": "/Calculator/static/css/main.923fa1e6.chunk.css"
+  },
+  {
+    "revision": "330e90facb553b195a8d",
+    "url": "/Calculator/static/js/2.6d0dfca5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.6d0dfca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f13930acf3e8170e41",
+    "url": "/Calculator/static/js/main.9b496b90.chunk.js"
+  },
+  {
+    "revision": "ef0a7bfe468712dc22fa",
+    "url": "/Calculator/static/js/runtime-main.512cadac.js"
+  }
+]);
